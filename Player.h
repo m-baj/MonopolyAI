09167a102402826b948c822364180d378e70237e
xDepcio <olek.drwal@gmail.com>
2024-11-03 20:41:59 +0100

    Added Player class

---
+++ Player.h
@@ -0,0 +1,25 @@
+//
+// Created by adrwal on 11/3/24.
+//
+
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <memory>
+#include <vector>
+
+#include "Property.h"
+
+
+class Player {
+private:
+    std::vector<std::shared_ptr<Property>> properties;
+    int money;
+    std::string name;
+
+    std::vector<std::shared_ptr<Utilities>> utilities;
+    std::vector<std::shared_ptr<Trains>> trains;
+};
+
+
+
+#endif //PLAYER_H
