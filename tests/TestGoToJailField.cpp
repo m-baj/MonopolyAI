87dfddf8fa227f7a2d87a21209c79842ec071d51
xDepcio <olek.drwal@gmail.com>
2024-11-06 15:08:53 +0100

    Added GoToJailField class

---
+++ tests/TestGoToJailField.cpp
@@ -0,0 +1,13 @@
+//
+// Created by adrwal on 11/6/24.
+//
+#include <gtest/gtest.h>
+
+#include "../src/GoToJailField.h"
+
+
+TEST(TestGoToJailField, CorrectInit)
+{
+    GoToJailField goToJailField("Test");
+    EXPECT_EQ(1, 1);
+}
