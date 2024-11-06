d2d9f86483d9bb7b007accfddae5b386852b08b8
xDepcio <olek.drwal@gmail.com>
2024-11-06 20:14:27 +0100

    Added test to getPlayersOnField for many players on the same field

--- tests/TestField.cpp
+++ tests/TestField.cpp
@@ -27,6 +27,59 @@ TEST(TestField, getPlayersOnField)
     EXPECT_EQ(field1->getPlayersOnField()[0].value(), board.getPlayers()[0].get());
 }
 
+TEST(TestField, getPlayersOnFieldManyPlayers)
+{
+    Board board;
+    auto field0 = std::make_shared<Field>("", board);
+    auto field1 = std::make_shared<Field>("", board);
+    board.pushField(field0);
+    board.pushField(field1);
+
+    auto player0 = std::make_unique<Player>("", 1);
+    auto player1 = std::make_unique<Player>("", 1);
+    auto player2 = std::make_unique<Player>("", 1);
+    auto player3 = std::make_unique<Player>("", 1);
+    player0->setPositionIdx(0);
+    player1->setPositionIdx(0);
+    player2->setPositionIdx(1);
+    player3->setPositionIdx(1);
+    board.pushPlayer(std::move(player0));
+    board.pushPlayer(std::move(player1));
+    board.pushPlayer(std::move(player2));
+    board.pushPlayer(std::move(player3));
+
+    auto field0Players = field0->getPlayersOnField();
+    auto field1Players = field1->getPlayersOnField();
+
+    EXPECT_EQ(field0Players.size(), 2);
+    EXPECT_EQ(field1Players.size(), 2);
+    EXPECT_EQ(field0Players[0].value(), board.getPlayers()[0].get());
+    EXPECT_EQ(field0Players[1].value(), board.getPlayers()[1].get());
+    EXPECT_EQ(field1Players[0].value(), board.getPlayers()[2].get());
+    EXPECT_EQ(field1Players[1].value(), board.getPlayers()[3].get());
+    board.setCurrentPlayerIndex(0);
+    board.movePlayer(1);
+    field0Players = field0->getPlayersOnField();
+    field1Players = field1->getPlayersOnField();
+    EXPECT_EQ(field0Players.size(), 1);
+    EXPECT_EQ(field1Players.size(), 3);
+    EXPECT_EQ(field0Players[0].value(), board.getPlayers()[1].get());
+    EXPECT_EQ(field1Players[0].value(), board.getPlayers()[0].get());
+    EXPECT_EQ(field1Players[1].value(), board.getPlayers()[2].get());
+    EXPECT_EQ(field1Players[2].value(), board.getPlayers()[3].get());
+    board.setCurrentPlayerIndex(3);
+    board.movePlayer(1);
+    field0Players = field0->getPlayersOnField();
+    field1Players = field1->getPlayersOnField();
+    EXPECT_EQ(field0Players.size(), 2);
+    EXPECT_EQ(field1Players.size(), 2);
+    EXPECT_EQ(field0Players[0].value(), board.getPlayers()[1].get());
+    EXPECT_EQ(field0Players[1].value(), board.getPlayers()[3].get());
+    EXPECT_EQ(field1Players[0].value(), board.getPlayers()[0].get());
+    EXPECT_EQ(field1Players[1].value(), board.getPlayers()[2].get());
+
+}
+
 TEST(TestField, getFieldIdx)
 {
     Board board;
